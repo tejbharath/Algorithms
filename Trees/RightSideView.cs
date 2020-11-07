ed2141fb5cfd53f0099909b5ca0db7e651a58222
tejbharat <tmujje@gmail.com>
2020-11-07 01:45:11 -0600

    Trees - Second Commit

---
+++ Trees/RightSideView.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trees
+{
+    public class RightSideView
+    {
+        public IList<int> RightView(TreeNode root)
+        {
+
+            IList<int> lst = new List<int>();
+            if (root == null)
+            {
+                return lst;
+            }
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count != 0)
+            {
+
+                var count = queue.Count;
+
+                for (int i = 0; i < count; i++)
+                {
+                    var curr = queue.Dequeue();
+
+                    if (curr.left != null)
+                    {
+                        queue.Enqueue(curr.left);
+                    }
+
+                    if (curr.right != null)
+                    {
+                        queue.Enqueue(curr.right);
+                    }
+
+                    if (i == count - 1)
+                    {
+                        lst.Add(curr.val);
+                    }
+                }
+
+            }
+            return lst;
+        }
+    }
+}
---
+++ Trees/ZigzagLevelOrder.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+namespace Trees
+{
+    public class ZigzagLevelOrder
+    {
+        public IList<IList<int>> ZigLevelOrder(TreeNode root)
+        {
+
+            IList<IList<int>> returnList = new List<IList<int>>();
+
+            if (root == null)
+            {
+                return returnList;
+            }
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            var flag = false;
+
+            while (queue.Count != 0)
+            {
+
+                var lst = new List<int>();
+                var count = queue.Count;
+
+
+                for (int i = 0; i < count; i++)
+                {
+
+                    var curr = queue.Dequeue();
+
+                    if (curr.left != null)
+                    {
+                        queue.Enqueue(curr.left);
+                    }
+
+                    if (curr.right != null)
+                    {
+                        queue.Enqueue(curr.right);
+                    }
+
+                    lst.Add(curr.val);
+
+                }
+
+                if (flag == true)
+                {
+                    lst.Reverse();
+                }
+
+                flag = !flag;
+                returnList.Add((IList<int>)lst);
+            }
+            return returnList;
+        }
+    }
+}

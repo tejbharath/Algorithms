e9884504677df20f7095a15c71ed9bda524c2813
tmujje <tmujje@gmail.com>
2021-04-22 01:21:36 -0400

    Added custom hashset and queue

---
+++ CustomDataStructures/MyHashSet.cs
@@ -0,0 +1,74 @@
+﻿namespace CustomDataStructures
+{
+    public class MyHashSet
+    {
+
+        private bool[][] arr;
+        private int buckets;
+        private int bucketItems;
+
+        /** Initialize your data structure here. */
+        public MyHashSet()
+        {
+            buckets = 1000;
+            bucketItems = 1000;
+            arr = new bool[buckets][];
+        }
+
+        public int Bucket(int key)
+        {
+            return key % buckets;
+        }
+
+        public int BucketItem(int key)
+        {
+            return key / bucketItems;
+        }
+
+        public void Add(int key)
+        {
+            var bucket = Bucket(key);
+            var bucketItem = BucketItem(key);
+            if (arr[bucket] == null)
+            {
+                if (bucket == 0)
+                {
+                    arr[bucket] = new bool[bucketItems + 1];
+                }
+                else
+                {
+                    arr[bucket] = new bool[bucketItems];
+                }
+            }
+            arr[bucket][bucketItem] = true;
+        }
+
+        public void Remove(int key)
+        {
+            var bucket = Bucket(key);
+            var bucketItem = BucketItem(key);
+            if (arr[bucket] == null) return;
+            arr[bucket][bucketItem] = false;
+        }
+
+        /** Returns true if this set contains the specified element */
+        public bool Contains(int key)
+        {
+            var bucket = Bucket(key);
+            var bucketItem = BucketItem(key);
+            if (arr[bucket] == null)
+            {
+                return false;
+            }
+            return arr[bucket][bucketItem];
+        }
+    }
+
+    /**
+     * Your MyHashSet object will be instantiated and called as such:
+     * MyHashSet obj = new MyHashSet();
+     * obj.Add(key);
+     * obj.Remove(key);
+     * bool param_3 = obj.Contains(key);
+     */
+}
---
+++ CustomDataStructures/MyQueue.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+
+namespace CustomDataStructures
+{
+    public class MyQueue
+    {
+
+        private Stack<int> firstStack;
+        private Stack<int> secondStack;
+
+        /** Initialize your data structure here. */
+        public MyQueue()
+        {
+            firstStack = new Stack<int>();
+            secondStack = new Stack<int>();
+
+        }
+
+        /** Push element x to the back of queue. */
+        public void Push(int x)
+        {
+            firstStack.Push(x);
+        }
+
+        /** Removes the element from in front of queue and returns that element. */
+        public int Pop()
+        {
+            Peek();
+            return secondStack.Pop();
+        }
+
+        /** Get the front element. */
+        public int Peek()
+        {
+            if (secondStack.Count == 0)
+            {
+                while (firstStack.Count != 0)
+                {
+                    secondStack.Push(firstStack.Pop());
+                }
+            }
+
+            return secondStack.Peek();
+        }
+
+        /** Returns whether the queue is empty. */
+        public bool Empty()
+        {
+            return firstStack.Count == 0 && secondStack.Count == 0;
+        }
+    }
+
+    /**
+     * Your MyQueue object will be instantiated and called as such:
+     * MyQueue obj = new MyQueue();
+     * obj.Push(x);
+     * int param_2 = obj.Pop();
+     * int param_3 = obj.Peek();
+     * bool param_4 = obj.Empty();
+     */
+}

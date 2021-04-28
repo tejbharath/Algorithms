54b4a0e1c4414292a653775f3b6a092bf78546aa
tmujje <tmujje@gmail.com>
2021-04-28 00:32:15 -0400

    more

--- Algorithms/MissingElement.cs
+++ Algorithms/MissingElement.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Algorithms
+﻿namespace Algorithms
 {
     public class MissingElement
     {
@@ -38,52 +32,24 @@ namespace Algorithms
             return -1;
         }
 
-        public int FindMissingElementBinarySearch(int[] nums)
+        public int FindMissingElementBinarySearch(int[] nums) //[1, 2, 3, 5, 6, 7]
         {
             var left = 0;
             var right = nums.Length - 1;
             while(left <= right)
             {
                 var mid = left + (right - left) / 2;
-                if (nums[mid] != nums[mid - 1] + 1)
+                if (nums[mid] != mid)
                 {
-                    right = mid;
-                }
-            }
-
-             
-            return -1;
-        }
-
-        private int BinarySearchRight(int left, int right, int[] nums)
-        {
-            while (left <= right)
-            {
-                var mid = left + (right - left) / 2;
-                if (nums[mid] != nums[mid + 1] - 1)
-                {
-                    return nums[mid+1] - 1;
+                    if(nums[mid-1] + 1 != mid)
+                    {
+                        return mid;
+                    }
+                    right = mid - 1;
                 }
                 else
                 {
-                    left = mid;
-                }
-            }
-            return -1;
-        }
-
-        private int BinarySearchLeft(int left, int right, int[] nums)
-        {
-            while (left <= right)
-            {
-                var mid = left + (right - left) / 2;
-                if (mid >= 0 || nums[mid] != nums[mid - 1] + 1)
-                {
-                    return nums[mid - 1] + 1;
-                }
-                else
-                {
-                    right = mid;
+                    left = mid + 1;
                 }
             }
             return -1;
